1f62ff69e4426c7df819eed9f186c13494586625
Matyas <burik.matyas@gmail.com>
2026-03-20 14:08:11 +0100

    Add files via upload

---
+++ IT2A_databeze/hra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT2A_databeze
+{
+    class hra
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Vydavatel { get; set; }
+        public int RokVydani { get; set; }
+        public string Typ {  get; set; }
+        public int Cena { get; set; }
+        public bool Vlastnena { get; set; }
+    }
+}
